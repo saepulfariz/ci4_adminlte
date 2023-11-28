cb519ba103e55e4db7c596275058acaf2fce99ec
saepulfariz <saepulfariz108@gmail.com>
2023-11-28 15:13:34 +0700

    first commit : register and login

---
+++ app/Config/Routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Auth::index');
+$routes->get('/auth', 'Auth::index');
+$routes->post('/auth', 'Auth::verify');
+$routes->get('/auth/register', 'Auth::register');
+$routes->post('/auth/register', 'Auth::registered');
+$routes->get('/auth/logout', 'Auth::logout');
+
+$routes->group('', ['filter' => '\App\Filters\AuthFilter'], function ($routes) {
+  $routes->get('dashboard', 'Dashboard::index');
+});
